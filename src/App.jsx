5ec2dfc7b935ca0c9284ce783bc005db02a8d98b
manojLovesCoding <mkum9417@gmail.com>
2024-11-26 16:08:32 +0530

    Testimonails Section Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
+import Testimonails from './components/Testimonails'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <About />
       <Projects />
+      <Testimonails />
     </div>
   )
 }
