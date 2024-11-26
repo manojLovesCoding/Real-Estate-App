ee1193c60727722e486f285abdc530cfaba25e2c
manojLovesCoding <mkum9417@gmail.com>
2024-11-26 19:22:21 +0530

    Framer Motion is JavaScript animation library used on all components

--- src/components/Testimonails.jsx
+++ src/components/Testimonails.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { assets, testimonialsData } from '../assets/assets'
+import { motion } from 'framer-motion'
 
 const Testimonails = () => {
     return (
-        <div className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id='Testimonials'>
+        <motion.div
+            initial={{ opacity: 0, x: 200 }}
+            transition={{ duration: 1 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true }}
+            className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id='Testimonials'>
             <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Customer <span className='underline-offset-4 decoration-1 under font-light'>Testimonails</span></h1>
             <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Real Stories from Those Who Found Home with Us</p>
 
@@ -22,7 +28,7 @@ const Testimonails = () => {
                     </div>
                 ))}
             </div>
-        </div>
+        </motion.div>
     )
 }
 
